01bacf1b7208b7f9ce5278ff0ba30cfc24ec28d8
Olav Krigolson <44245195+krigolson@users.noreply.github.com>
2024-10-24 14:16:56 -0700

    Update

---
+++ filterData/doFilterLow.m
@@ -0,0 +1,13 @@
+function EEG = doFilterLow(EEG,filterParameters)
+
+    % currently expects an EEGLAB data structure
+
+    %% Apply a bandpass filter
+    EEG = pop_eegfiltnew(EEG, filterParameters.low, []);
+    
+    %% 60 Hz Notch filter
+    %EEG = pop_eegfiltnew(EEG, filterParameters.notch-2, filterParameters.notch+2,[],1);
+
+    %EEG.filterParameters = filterParameters;
+
+end
