5b9f30f02b7b896fa6eecb13428b0e8255e66d82
Olav Krigolson <44245195+krigolson@users.noreply.github.com>
2025-07-10 07:43:03 -0700

    Update

--- epochData/doEpochData.m
+++ epochData/doEpochData.m
@@ -8,9 +8,12 @@ function EEG = doEpochData(EEG,epochMarkers,epochTimes)
     % epochMarkers = {'S  5','S 15','S 25','S 35'};
     % use times in the following format in ms which the function will
     % convert to seconds for you. So, to get -200 to 600 use -200 to 600.
+    % Note, you can work in seconds as well.
 
-    % covert to seconds
-    epochTimes = epochTimes / 1000;
+    % covert to seconds if needed
+    if abs(epochTimes(1)) > 100
+        epochTimes = epochTimes / 1000;
+    end
 
     % call EEGLAB function
     EEG = pop_epoch(EEG,epochMarkers,epochTimes);
