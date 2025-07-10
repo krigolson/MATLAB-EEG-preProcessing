13bc99fe0d7270de69ed111e6042adb1764615a7
Olav Krigolson <44245195+krigolson@users.noreply.github.com>
2025-07-09 18:06:28 -0700

    Updates

--- baselineData/doBaseline.m
+++ baselineData/doBaseline.m
@@ -1,7 +1,7 @@
 function inputData = doBaseline(inputData,baselineWindow)
 
     % written as a shell by Olav Krigolson for consitency
-    % expects EEG in EEGLAB format and a baseline window in s [-.2 0]
+    % expects EEG in EEGLAB format and a baseline window in s [-200 0]
     
     try
     
@@ -9,8 +9,10 @@ function inputData = doBaseline(inputData,baselineWindow)
         
     catch
         
-        baselineWindow = baselineWindow / 1000;
-        inputData = pop_rmbase(inputData,baselineWindow);
+        %baselineWindow = baselineWindow / 1000;
+        %inputData = pop_rmbase(inputData,baselineWindow);
+
+        disp('FAIL');
         
     end
 
