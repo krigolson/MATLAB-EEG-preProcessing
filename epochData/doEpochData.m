eaa6f070bbbaae87b5f47470c9e0930dbd6fa272
Olav Krigolson <44245195+krigolson@users.noreply.github.com>
2026-03-12 11:22:01 -0700

    Update

--- epochData/doEpochData.m
+++ epochData/doEpochData.m
@@ -17,6 +17,39 @@ function EEG = doEpochData(EEG,epochMarkers,epochTimes)
 
     % call EEGLAB function
     EEG = pop_epoch(EEG,epochMarkers,epochTimes);
+
+    % -------------------------------------------------------------------------
+    % Step 1: Keep only the time-locking event (latency == 0) in each epoch
+    % -------------------------------------------------------------------------
+    for i = 1:length(EEG.epoch)
+        lats = cell2mat(EEG.epoch(i).eventlatency);
+        idx  = find(lats == 0);
+    
+        EEG.epoch(i).event         = EEG.epoch(i).event(idx);
+        EEG.epoch(i).eventlatency  = EEG.epoch(i).eventlatency(idx);
+        EEG.epoch(i).eventtype     = EEG.epoch(i).eventtype(idx);
+        EEG.epoch(i).eventduration = EEG.epoch(i).eventduration(idx);
+        EEG.epoch(i).eventchannel  = EEG.epoch(i).eventchannel(idx);
+        EEG.epoch(i).eventbvtime   = EEG.epoch(i).eventbvtime(idx);
+        EEG.epoch(i).eventbvmknum  = EEG.epoch(i).eventbvmknum(idx);
+        EEG.epoch(i).eventcode     = EEG.epoch(i).eventcode(idx);
+    
+        % Handle eventurvent if present
+        if isfield(EEG.epoch(i), 'eventurvent')
+            EEG.epoch(i).eventurvent = EEG.epoch(i).eventurvent(idx);
+        end
+    end
+    
+    % -------------------------------------------------------------------------
+    % Step 2: Unwrap remaining 1x1 cells to plain scalars
+    % -------------------------------------------------------------------------
+    for i = 1:length(EEG.epoch)
+        EEG.epoch(i).eventlatency  = cell2mat(EEG.epoch(i).eventlatency);
+        EEG.epoch(i).eventduration = cell2mat(EEG.epoch(i).eventduration);
+        EEG.epoch(i).eventchannel  = cell2mat(EEG.epoch(i).eventchannel);
+        EEG.epoch(i).eventbvtime   = cell2mat(EEG.epoch(i).eventbvtime);
+        EEG.epoch(i).eventbvmknum  = cell2mat(EEG.epoch(i).eventbvmknum);
+    end
     
     % store some stuff
     EEG.epochMarkers = epochMarkers;
